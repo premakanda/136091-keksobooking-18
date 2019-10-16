7933853b58a0c24882cbb0becb53d4feffd6a6d1
Piotr Yanushkevich <1999301py@gmail.com>
2019-10-16 16:16:20 +0300

    задание модуляция

---
+++ js/pin.js
@@ -0,0 +1,72 @@
+'use strict';
+
+(function () {
+
+  var userMapPins = document.querySelector('.map__pins');
+  var userPinsTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+
+  var getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  var getRandomItem = function (arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  };
+
+  var generatePins = function (count) {
+    var data = [];
+    for (var i = 0; i < count; i++) {
+      data.push({
+        author: {
+          avatars: 'img/avatars/user0' + (i + 1) + '.png'
+        },
+        offer: {
+          title: 'Заголовок предложения',
+          address: '600, 350',
+          price: getRandomItem(window.data.priceIndex),
+          type: getRandomItem(window.data.TYPES),
+          rooms: getRandomItem(window.data.roomsIndex),
+          guests: getRandomItem(window.data.guestsIndex),
+          checkin: getRandomItem(window.data.CHECKINS),
+          checkout: getRandomItem(window.data.CHECKOUTS),
+          features: window.data.FEATURES.slice(getRandomInt(0, window.data.FEATURES.length)),
+          description: 'Описание',
+          photos: window.data.PHOTOS.slice(getRandomInt(0, window.data.PHOTOS.length)),
+        },
+        location: {
+          x: getRandomInt(0, window.data.userMap.offsetWidth),
+          y: getRandomInt(130, 630)
+        }
+      });
+    }
+    return data;
+  };
+
+  var renderPin = function (obj) {
+    var pinElement = userPinsTemplate.cloneNode(true);
+    pinElement.style.left = obj.location.x + 'px';
+    pinElement.style.top = obj.location.y + 'px';
+    pinElement.querySelector('img').src = obj.author.avatars;
+
+    pinElement.addEventListener('click', function () {
+      window.openCard(obj);
+    });
+
+    return pinElement;
+  };
+
+  var renderMapPins = function (arr) {
+    var fragment = document.createDocumentFragment();
+    for (var t = 0; t < arr.length; t++) {
+      fragment.appendChild(renderPin(arr[t]));
+    }
+    userMapPins.appendChild(fragment);
+  };
+
+  var pins = generatePins(8);
+  renderMapPins(pins);
+
+})();
